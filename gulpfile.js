4fec7633ef74b6ab7e57bcde0a9cbbec7efbade3
Ivan-Starykh <izobilny2016@yandex.ru>
2024-12-04 20:42:58 +0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp');
-const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
@@ -7,9 +6,13 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const mediaquery = require('postcss-combine-media-query');
 const cssnano = require('cssnano');
-const htmlMinify = require('html-minifier');
+const htmlMinifier = require('html-minifier').minify;
 const gulpPug = require('gulp-pug');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const through2 = require('through2');
+const fonter = require('gulp-fonter');
+const replace = require('gulp-replace');
 
 function serve() {
   browserSync.init({
@@ -19,80 +22,115 @@ function serve() {
   });
 }
 
+function updatePaths() { 
+  return gulp
+    .src('dist/**/*.html')
+    .pipe(plumber())
+    .pipe(replace('./fonts/fonts.css', '/fonts/fonts.css'))
+    .pipe(replace('./images/favicon.ico', '/images/favicon.ico'))
+    .pipe(replace('./images/favicon.svg', '/images/favicon.svg'))
+    .pipe(replace('./images/apple-touch-icon.png', '/images/apple-touch-icon.png'))
+    .pipe(replace('./css/variables.css', '/css/variables.css'))
+    .pipe(replace('./css/globals.css', '/css/globals.css'))
+    .pipe(replace('./css/style.css', '/css/style.css'))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+}
+
 function layoutsScss() {
   const plugins = [autoprefixer(), mediaquery(), cssnano()];
   return gulp
     .src('src/layouts/**/*.scss')
-    .pipe(sass())
-    .pipe(concat('bundle.css'))
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
-    .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
 }
 
 function pagesScss() {
   const plugins = [autoprefixer(), mediaquery(), cssnano()];
   return gulp
     .src('src/pages/**/*.scss')
-    .pipe(sass())
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
-    .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
 }
 
 function pug() {
   return gulp
     .src('src/pages/**/*.pug')
-    .pipe(
-      gulpPug({
-        pretty: true,
-      })
-    )
-    .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(plumber())
+    .pipe(gulpPug({ pretty: true }))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
 }
 
 function html() {
   const options = {
     removeComments: true,
+    collapseWhitespace: true,
     removeRedundantAttributes: true,
+    useShortDoctype: true,
+    removeEmptyAttributes: true,
     removeScriptTypeAttributes: true,
     removeStyleLinkTypeAttributes: true,
-    sortClassName: true,
-    useShortDoctype: true,
-    collapseWhitespace: true,
     minifyCSS: true,
+    minifyJS: true,
     keepClosingSlash: true,
+    sortClassName: true
   };
   return gulp
     .src('src/**/*.html')
     .pipe(plumber())
-    .on('data', function (file) {
-      const buferFile = Buffer.from(
-        htmlMinify.minify(file.contents.toString(), options)
-      );
-      return (file.contents = buferFile);
-    })
-    .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(through2.obj(function (file, _, cb) {
+      if (file.isBuffer()) {
+        const contents = file.contents.toString();
+        file.contents = Buffer.from(htmlMinifier(contents, options));
+      }
+      cb(null, file);
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+}
+
+function scripts() {
+  return gulp
+    .src('src/js/**/*.js')
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream());
 }
 
 function css() {
   const plugins = [autoprefixer(), mediaquery(), cssnano()];
   return gulp
-    .src('src/**/*.css')
+    .src('src/css/**/*.css')
     .pipe(plumber())
-    .pipe(concat('bundle.css'))
     .pipe(postcss(plugins))
-    .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
 }
 
 function images() {
   return gulp
     .src('src/**/*.{jpg,png,svg,gif,ico,webp,avif}')
     .pipe(gulp.dest('dist/images'))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.stream());
+}
+
+async function fonts() { 
+  const ttf2woff2 = await import('gulp-ttf2woff2'); 
+  return gulp
+    .src('src/fonts/*.{ttf,otf}')
+    .pipe(plumber())
+    .pipe(ttf2woff2.default())
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(browserSync.stream());
 }
 
 function clean() {
@@ -100,28 +138,28 @@ function clean() {
 }
 
 function watchFiles() {
-  gulp.watch(['src/**/*.pug'], pug);
-  gulp.watch(['src/**/*.html'], html);
-  gulp.watch(['src/**/*.css'], css);
-  gulp.watch(['src/layouts/**/*.scss'], layoutsScss);
-  gulp.watch(['src/pages/**/*.scss'], pagesScss);
-  gulp.watch(['src/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch('src/**/*.pug', pug);
+  gulp.watch('src/**/*.html', html);
+  gulp.watch('src/css/**/*.css', css);
+  gulp.watch('src/layouts/**/*.scss', layoutsScss);
+  gulp.watch('src/pages/**/*.scss', pagesScss);
+  gulp.watch('src/js/**/*.js', scripts);
+  gulp.watch('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}', images);
+  gulp.watch('src/fonts/*.{ttf,otf}', fonts);
 }
 
-const build = gulp.series(
-  clean,
-  gulp.parallel(pug, layoutsScss, pagesScss, images)
-);
+const build = gulp.series(clean, gulp.parallel(pug, html, layoutsScss, pagesScss, css, scripts, images, fonts));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
-exports.html = html;
 exports.pug = pug;
+exports.html = html;
 exports.css = css;
 exports.layoutsScss = layoutsScss;
 exports.pagesScss = pagesScss;
+exports.scripts = scripts;
 exports.images = images;
+exports.fonts = fonts;
 exports.clean = clean;
-
 exports.build = build;
 exports.watchapp = watchapp;
 exports.default = watchapp;
